6c3c10c1d4dd5279acf1a2aa2342e64e163afd21
Tamagoch1 <45555202+Tamagoch1@users.noreply.github.com>
2020-03-29 17:30:14 +0500

    Randomizer

---
+++ random.js
@@ -0,0 +1,16 @@
+var games = [
+	"Pummel Party",
+	"Human Fall Flat",
+	"Gang Beasts",
+	"Ultimate Chicken Horse",
+	"Stick Fight: The Game",
+	"Witch It",
+	"Overcooked",
+	"Move or Die",
+	"BattleBlock Theater",
+	"Keep Talking and Nobody Explodes",
+	"Borderlands 2",
+	"Monaco"
+];
+
+games[Math.floor(Math.random() * games.length)];
